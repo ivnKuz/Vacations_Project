be93b0984ae2dd12e99592eaeb1b5712ccc16a2e
CyberProger <booleansandwich@gmail.com>
2024-04-09 01:48:20 +0300

    sorted dates

--- frontend/src/components/home/home/Home.tsx
+++ frontend/src/components/home/home/Home.tsx
@@ -11,11 +11,15 @@ import Vacation from "../../../models/vacation";
 function Home(): JSX.Element {
     const [vacations, setVacations] = useState<Vacation[]>([]);
     useEffect(()=>{
-        vacationsService.getAll().then(serverVacations => setVacations(serverVacations)).catch()
-    },[])
+        vacationsService.getAll().then(serverVacations => setVacations(serverVacations)).catch();
+    },[]);
     return (
         <div className="Home">
-               {vacations.map((vacation, indx) => <Card key={indx} vacation={vacation}/>)}
+               {vacations.sort((a,b) => {
+            let firstDate = a.startDate as unknown as Date;
+            let secondDate = b.startDate as unknown as Date;
+            return firstDate > secondDate ?  1 :  -1;
+        }).map((vacation, indx) => <Card key={indx} vacation={vacation}/>)}
                
         </div>
     );
