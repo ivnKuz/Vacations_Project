d3889b19f9ced0bf36cd181aab98e59b5feedb33
CyberProger <booleansandwich@gmail.com>
2024-04-20 23:58:42 +0300

    fixed the state problem, now followers count properly when clicked, but code is probably bad

--- frontend/src/components/home/card/card.tsx
+++ frontend/src/components/home/card/card.tsx
@@ -7,14 +7,12 @@ import User from "../../../models/User";
 import vacationsService from "../../../services/Vocations";
 import follower from "../../../models/follower";
 import { useEffect, useState } from "react";
-import vacations from "../../../services/Vocations";
 import followerCount from "../../../models/followerCount";
 interface vacationCardProps {
     vacation: Vacation;
     user: User | undefined;
     follows: follower[];
     vocationFollowers: followerCount;
-    // setFollowerCount: React.Dispatch<React.SetStateAction<followerCount[]>>;
 }
 function Card(props:vacationCardProps): JSX.Element {
     const [follower, setFollower] = useState<follower>();
@@ -27,18 +25,31 @@ function Card(props:vacationCardProps): JSX.Element {
 
     //maybe make getFollowers and if vocationId for this userId is there set followed to true <---
     
-     async function follow(){
+    async function follow(){
+       
+        // setNumberOfFollowers(props.vocationFollowers.followers);
         
         followed ? setFollowed(false) : setFollowed(true)
-     
         //thought to set it on when follow button pressed again
-        if(!followed) vacations.addFollower(follower);
+        if(!followed) {
+            await vacationsService.addFollower(follower);
+            await getFollowerCount();
+        }
         // TO CHANGE, it deletes every vocation with this Id
-        if(followed) vacations.deleteFollow(follower?.vocationId, follower?.userId)
+        if(followed) { 
+            await vacationsService.deleteFollow(follower?.vocationId, follower?.userId)
+            await getFollowerCount();
+        }
         //wanted to set state from here up, didnt work
-        // const updatedFollowerCount = await vacationsService.getFollowerCount();
-        // props.setFollowerCount(updatedFollowerCount)
-        setNumberOfFollowers(props.vocationFollowers.followers)
+        
+    }
+    async function getFollowerCount(){
+        vacationsService.getFollowerCount().then(updatedFollowerCount => {
+            // props.setFollowerCount(updatedFollowerCount)
+            setNumberOfFollowers(updatedFollowerCount.find(item => item.id === follower?.vocationId)?.followers);
+            console.log(updatedFollowerCount.find(item => item.id === follower?.vocationId)?.followers);
+            
+        }).catch(err => console.log(err));
     }
 //save followers to redux ?????
      function  checkFollowedVocations(){  
@@ -67,7 +78,7 @@ function Card(props:vacationCardProps): JSX.Element {
             
             <div className="card-body">
                 <div className="first-layer">
-                     {dayjs(props.vacation.startDate?.toString()).format('YYYY MM-DD')} - {dayjs(props.vacation.endDate?.toString()).format('YYYY MM-DD')}
+                     {dayjs(props.vacation.startDate?.toString()).format('DD.MM.YYYY')} - {dayjs(props.vacation.endDate?.toString()).format('DD.MM.YYYY')}
                  </div>
                 <div className="second-layer">
                 {props.vacation.description}
